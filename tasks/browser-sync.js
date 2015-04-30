61bfe79cbe891c99e9ec12b2327e68edb2e1c3e5
Ben King <ben@designbybenking.co.uk>
2015-04-30 19:47:53 +0100

    Add missing space to task function

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -2,12 +2,12 @@ var browserSync = require( 'browser-sync' );
 var hygienist = require( 'hygienist-middleware' );
 
 module.exports = function( gulp, plugins, path ) {
-    gulp.task('browser-sync', 'serves the build using BrowserSync', function() {
+    gulp.task( 'browser-sync', 'serves the build using BrowserSync', function() {
         browserSync({
             server: {
                 baseDir: "./build",
                 middleware: hygienist("./build")
             }
         });
-    });
+    } );
 };
